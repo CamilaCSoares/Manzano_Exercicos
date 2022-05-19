c02a0a0350a09c21247c47d75257d7265a6eea09
Camila Carla Soares <camilacarlasoaress@gmail.com>
2022-05-19 01:00:18 -0300

    Add Exercícios

--- Manzano_JavaScript/46 b/manzano.js
+++
@@ -1,9 +0,0 @@
-let contador = 1;
-let acumulador =0;
-
-while (contador <=100) {
-    acumulador = acumulador+contador;
-    contador++;
-}
-
-alert(acumulador);
--- Manzano_JavaScript/46_A__Tabuada/script.js
+++
@@ -1,9 +0,0 @@
-let contador, numero, resultado
-contador = 1
-numero = parseInt (prompt("Digte um número para ter sua tabuada: "))
-
-while (contador <= 10) {
-resultado = numero * contador
-alert (numero + " X " + contador + " = " + resultado)
-contador = contador + 1
-}
